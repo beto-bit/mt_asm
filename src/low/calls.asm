80131b15a1d47c1e39af8ce36dae7659b6c6b737
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-09-21 11:19:34 -0600

    Added some comments

--- src/low/calls.asm
+++ src/low/calls.asm
@@ -1,15 +1,22 @@
 global write, exit
 global mmap, munmap
 
+; Basic utilities
 %define SYS_WRITE 1
 %define SYS_EXIT 60
+
+; Memory
 %define SYS_MMAP 9
 %define SYS_MUNMAP 11
 
 section .text
 
-; does the same as libc equivalent
+; Does the same as libc equivalent
 ; ssize_t write(int fd, const void *buf, size_t count)
+;
+; rdi: The File Descriptor
+; rsi: The buffer passed to it
+; rdx: The amount to be written
 write:
     mov eax, SYS_WRITE
     syscall
@@ -22,6 +29,7 @@ exit:
     hlt
 
 ; raw mmap syscall
+; the same signature as libc equivalent
 mmap:
     mov eax, SYS_MMAP
     mov r10, rcx    ; the only different register
@@ -29,7 +37,9 @@ mmap:
     ret
 
 ; raw mmunmap syscall
+; the same signature as libc equivalent
 munmap:
     mov eax, SYS_MUNMAP
     syscall
     ret
+
