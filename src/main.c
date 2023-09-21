610894e66de5c1f65861a3ec8a2990ffbe4c3fd8
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-09-21 09:02:29 -0600

    Removed unused includes

--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 #include <stddef.h>
-#include <stdint.h>
-#include "calls.h"
 #include "fmt/print.h"
-#include "utils.h"
 
 int main(size_t argc, char *argv[]) {
     for (size_t idx = 0; idx < argc; ++idx) {
