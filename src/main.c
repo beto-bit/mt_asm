a3b6a2c56c310245c64d19618daa6f1f3fccfec5
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-09-22 19:15:55 -0600

    Show atomic vs non atomic

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdatomic.h>
 
 #include "fmt/print.h"
 #include "thr/thread.h"
 
 
 #define NO_THREADS 10
+atomic_int atm_count = 0;
 int count = 0;
 
 
@@ -13,6 +15,7 @@ int f(void *arg) {
     int* inc = (int*) arg;
 
     for (int i = 0; i < 1000000; ++i) {
+        atm_count += *inc;
         count += *inc;
     }
 
@@ -23,7 +26,7 @@ int f(void *arg) {
 int main(void) {
     struct Thread thrd[NO_THREADS];
 
-    int inc = 10;
+    int inc = 1;
 
     // Create threads
     for (size_t i = 0; i < NO_THREADS; ++i)
@@ -33,7 +36,11 @@ int main(void) {
     for (size_t i = 0; i < NO_THREADS; ++i)
         clean_thread(thrd + i);
 
-    print_str("Total count: ");
+    print_str("Atomic count:\t\t");
+    format_num(atm_count, DEC);
+    print_char('\n');
+
+    print_str("Non-atomic count:\t");
     format_num(count, DEC);
     print_char('\n');
 
