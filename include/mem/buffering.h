3f6ddd137d7a3599447cb0daadd13dc21847a9d3
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-09-25 17:53:02 -0600

    Added some bufferint utilities

---
+++ include/mem/buffering.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+struct MemBuff {
+    uint8_t *buff;
+    size_t length;
+    size_t capacity;
+};
+
+size_t remaining_bytes(struct MemBuff *membuff);
+
+/// Appends a single byte. Returns false on error.
+bool append_byte(struct MemBuff *membuff, uint8_t byte);
+
+/// Returns the remaining number of bytes to append
+size_t append_buff(struct MemBuff *membuff, uint8_t *buff, size_t size);
+
---
+++ src/mem/buffering.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "mem/buffering.h"
+
+size_t remaining_bytes(struct MemBuff *membuff) {
+    return membuff->capacity - membuff->length;
+}
+
+bool append_byte(struct MemBuff *membuff, uint8_t byte) {
+    if (remaining_bytes(membuff) > 0 ) {
+        // Beacause is postfix++, the length gets incremented later
+        membuff->buff[membuff->length++] = byte;
+        return true;
+    }
+
+    return false;
+}
+
+size_t append_buff(struct MemBuff *membuff, uint8_t *buff, size_t size) {
+    size_t idx;
+
+    // Copy the bytes
+    for (idx = 0; membuff->length < membuff->capacity; ++membuff->length) {
+        // Check buffer boundaries
+        if (idx >= size) break;
+
+        // Storing to mbuff[len] because len always points to one element above
+        membuff->buff[membuff->length] = buff[idx];
+        ++idx;
+    }
+
+    return size - idx;
+}
