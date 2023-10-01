87fe85c9aec16b34698849ddd1e4af589a7ece6e
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-09-30 22:53:20 -0600

    Working join

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,21 @@
 
 #include "fmt/print.hpp"
 #include "sync/thread.hpp"
+#include "sync/time.hpp"
 
-int foo(int num);
+int foo(int num) {
+    printfmt<DEC>("From another thread: {}\n", num);
+    syn::sleep(0.5);
+
+    return 0;
+}
 
 int main(std::span<std::string_view> args) {
-    syn::SimThread thrd{ foo, 9 };
+    syn::SimThread thrd{ foo,42 };
+    
+    printfmt<DEC>("TID: {}\n", thrd.get_tid());
+
+    thrd.join();
 
     return 0;
 }
