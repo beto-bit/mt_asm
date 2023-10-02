2727dbfd4d66fac4c5644f318778985ced0399c1
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-10-02 10:17:28 -0600

    Revert "stoi implementation but constexpr"
    This reverts commit 00951543cb9b48194107e8d3172fd6c705cf10a4.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <span>
 #include <string_view>
 
-#include "fmt/string.hpp"
 #include "fmt/print.hpp"
+#include "sync/thread.hpp"
+#include "sync/time.hpp"
+
+int foo(int num) {
+    printfmt<DEC>("From another thread: {}\n", num);
+    syn::sleep(0.5);
+
+    return 0;
+}
 
 int main(std::span<std::string_view> args) {
+    syn::SimThread thrd{ foo,42 };
 
-    constexpr auto maybe_num1 = str_to_int("0x69");
-    constexpr auto maybe_num2 = str_to_int("-13");
-    constexpr auto maybe_num3 = str_to_int("42");
+    printfmt<DEC>("TID: {}\n", thrd.get_tid());
 
-    printfmt<HEX>("{}\n", maybe_num1.value_or(0));
-    printfmt<DEC>("{}\n", maybe_num2.value_or(0));
-    printfmt<DEC>("{}\n", maybe_num3.value_or(0));
+    thrd.join();
 
     return 0;
 }
