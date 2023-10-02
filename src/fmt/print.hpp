cd40a6cac1f79e7e0a5bf4c231c825ce681da3c5
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-10-02 10:17:29 -0600

    Revert "Constexpr Formatting"
    This reverts commit edce2357a6b81e4f6f6d140da3378a380c0593d7.

--- src/fmt/print.hpp
+++ src/fmt/print.hpp
@@ -3,12 +3,8 @@
 #include <array>
 #include <concepts>
 #include <cstddef>
-#include <cstdint>
 #include <string_view>
 #include <type_traits>
-
-#include "math/util.hpp"
-
 
 // Basic printing
 void print(char c);
@@ -35,80 +31,17 @@ struct HEX {
     static constexpr size_t size = 16;
 };
 
-template<typename T>
-concept is_format_kind = std::is_same_v<T, DEC> || std::is_same_v<T, HEX>;
-
-
-/// Returns the number of digits of a number
-/// In the case of DEC, includes the '-' sign.
-template<typename T>
-requires is_format_kind<T>
-constexpr std::uint8_t no_digits(std::integral auto num) {
-    if constexpr (std::is_same_v<T, DEC>)
-    {
-        const bool is_negative = (num < 0) ? true : false;
-        const std::uint8_t digits = math::integer_log(math::abs(num), 10u);
-
-        return digits + is_negative;
-    }
-    else {
-        return math::integer_log(math::abs(num), 16u);
-    }
-}
-
 /// Returns an array of characters depending on the specified
 /// format type.
 /// The array is of size 20 if DEC, 16 if HEX
 template<typename T>
-requires is_format_kind<T>
-constexpr std::array<char, T::size>
-format_num(std::integral auto num)
-{
-    if constexpr (std::is_same_v<T, DEC>)
-    {
-        std::array<char, 20> buff{};
-        const bool is_negative = (num < 0) ? true : false;
-
-        // Reverse enter characters in the buffer
-        size_t index = no_digits<DEC>(num) - 1;
-        num = math::abs(num);
-
-        while (true) {
-            auto div = num / 10;
-            auto mod = num % 10;
-
-            buff[index] = num_to_hex(mod);
-
-            if (!div) break;
-
-            --index;
-            num = div;
-        }
-
-        if (is_negative)
-            buff[--index] = '-';
-
-        return buff;
+requires std::is_same_v<T, DEC> || std::is_same_v<T, HEX>
+std::array<int, T::size>
+killme() {
+    if constexpr (std::is_same_v<T, DEC>) {
+        return {0, 1};
     }
-    else
-    {
-        std::array<char, 16> buff{};
-
-        size_t index = no_digits<HEX>(num) - 1;
-
-        while (true) {
-            auto div = num / 16;
-            auto mod = num % 16;
-
-            buff[index] = num_to_hex(mod);
-
-            if (!div) break;
-
-            --index;
-            num = div;
-        }
-
-        return buff;
+    else {
+        return {5};
     }
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,26 @@
-#include <cmath>
-#include <span>
-
-#include "math/util.hpp"
 #include "fmt/print.hpp"
+#include <span>
 
 struct Shitty {};
 
-void print_members(std::span<const char> spn) {
-    for (char c : spn) {
-        print(c);
-        print(',');
+void print_members(std::span<const int> spn) {
+    for (int num : spn) {
+        print(num_to_hex(num));
+        print(' ');
     }
     print('\n');
 }
 
 int main() {
-    constexpr auto res2 = math::integer_log(0x1ffu, 16u);
-    constexpr auto res1 = math::integer_log(999u, 10u);
-    constexpr auto res = no_digits<DEC>(-100);
-    // constexpr auto res6 = no_digits<HEX>(0xabcd);
-
-    constexpr auto dec_fmt = format_num<DEC>(99);
-    constexpr auto hex_fmt = format_num<HEX>(0xaaaabbbbccccdddd);
+    auto dec_fmt = killme<DEC>();
+    auto hex_fmt = killme<HEX>();
+    // auto juan = killme<Shitty>();
 
     print_members(dec_fmt);
     print_members(hex_fmt);
 
+    // print_members(juan);
+        
     return 0;
 }
 
