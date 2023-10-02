1c8b7c85f4e01db6326d37c91658d9b572f3b805
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-10-02 10:17:29 -0600

    Revert "Jason Turner, this is for you"
    This reverts commit 258eed912ead8b0c3faee68320c30e7e2f4481ad.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
-#include <span>
-#include <string_view>
-
 #include "fmt/print.hpp"
 
-int main(std::span<const std::string_view> args) {
-
-    for (auto str : args) {
-        println(str);
-    }
-
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+    printfmt<DEC>("Hi mum. My magic number is: {}\n", 42);
     return 0;
 }
--- unlibc++/x86_64/entry.cpp
+++ unlibc++/x86_64/entry.cpp
@@ -1,18 +1,5 @@
-#include <array>
-#include <cstddef>
-#include <span>
-#include <string_view>
-
-int main(std::span<const std::string_view> args);
+int main(int argc, char* argv[]);
 
 extern "C" int cpp_entry(int argc, char* argv[]) {
-    const size_t span_size = static_cast<size_t>(argc);
-
-    std::array<std::string_view, 255> args;
-
-    for (size_t idx = 0; idx < span_size; ++idx) {
-        args[idx] = std::string_view{argv[idx]};
-    }
-
-    return main({args.data(), span_size});
+    return main(argc, argv);
 }
