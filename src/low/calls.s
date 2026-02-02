f5c8373798c0c28beed9738b33823c7e1acbfcc5
beto-bit <76757367+beto-bit@users.noreply.github.com>
2026-02-02 17:18:53 -0600

    quickfix

--- src/low/calls.s
+++ src/low/calls.s
@@ -14,7 +14,7 @@
 .equ SYS_CLONE,     120
 .equ SYS_CLONE3,    435
 
-.equ SYS_NANOSLEEP, 162
+.equ SYS_NANOSLEEP, 101
 .equ SYS_FUTEX,     98
 
 
@@ -32,3 +32,37 @@ exit:
     mov x8, SYS_EXIT
     svc 0
     hlt 0
+
+mmap:
+    mov x8, SYS_MMAP
+    svc 0
+    ret
+
+munmap:
+    mov x8, SYS_MUNMAP
+    svc 0
+    ret
+
+nanosleep:
+    mov x8, SYS_NANOSLEEP
+    svc 0
+    ret
+
+bare_clone:
+    ret
+
+bare_clone2:
+    ret
+
+// Clone 3 copy from glibc: https://codebrowser.dev/glibc/glibc/sysdeps/unix/sysv/linux/aarch64/clone3.S.html
+// x0: struct clone_args *cl_args
+// x1: size_t size
+// x2: int function(void*)
+// x3: void* arg
+clone3:
+    ret
+
+futex:
+    mov x8, SYS_FUTEX
+    svc 0
+    ret
